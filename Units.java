d9fa5b21cafa174eaf0114c5385940ed90fcb5f5
faith002-star <faey36312@gmail.com>
2026-02-18 14:10:35 +0300

    Fix spelling errors in Units.java
    Corrected spelling errors in unit names.

--- Units.java
+++ Units.java
@@ -9,10 +9,11 @@ public class Units {
         
         // Displaying each unit on a new line
         System.out.println("1. Object Oriented Programming 2");
-        System.out.println("2. Network systes administration");
+        System.out.println("2. Network systems administration");
         System.out.println("3. Mobile Computing");
         System.out.println("4. Cloud Computing");
         System.out.println("5. Application Programming");
-        System.out.println("6. Object Oriented Analysis and Designs");
+        System.out.println("6. Object Oriented Analysis and Design");
     }
+
 }
