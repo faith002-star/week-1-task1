c0656808cdc204be85c58dd076a1f337d8ff6392
faith002-star <faey36312@gmail.com>
2026-02-18 14:05:47 +0300

    Add files via upload

---
+++ Units.java
@@ -0,0 +1,18 @@
+/**
+ * Units.java
+ * This program displays a list of academic units offered this semester.
+ */
+public class Units {
+    public static void main(String[] args) {
+        // Displaying the header
+        System.out.println("Units Offered This Semester:");
+        
+        // Displaying each unit on a new line
+        System.out.println("1. Object Oriented Programming 2");
+        System.out.println("2. Network systes administration");
+        System.out.println("3. Mobile Computing");
+        System.out.println("4. Cloud Computing");
+        System.out.println("5. Application Programming");
+        System.out.println("6. Object Oriented Analysis and Designs");
+    }
+}
